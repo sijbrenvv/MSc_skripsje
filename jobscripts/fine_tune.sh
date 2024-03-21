23e64a51004bc760dd9be37d9de4b47f08e05fff
S4410653 <sijbren.vv@outlook.com>
2024-03-21 20:51:23 +0100

    Update fine_tune.sh: Formatting

--- jobscripts/fine_tune.sh
+++ jobscripts/fine_tune.sh
@@ -7,15 +7,19 @@
 module purge
 module load Python/3.11.3-GCCcore-12.3.0
 
+# Create and load virtual environment
+python3 -m venv $HOME/venvs/master_thesis
+source $HOME/venvs/master_thesis/bin/activate
+
+# Install dependencies
+pip install --upgrade pip wheel setuptools
+pip install -r requirements.txt
+
 # move cached datasets to the /scratch directory
 export HF_DATASETS_CACHE="/scratch/$USER/.cache/huggingface/datasets"
 
 # move downloaded models and tokenizers to the /scratch directory
 export TRANSFORMERS_CACHE="/scratch/$USER/.cache/huggingface/hub"
-
-python3 -m venv $HOME/venvs/master_thesis
-
-source $HOME/venvs/master_thesis/bin/activate
 
 # Bleu
 python3 model_behaviour.py -tr data/synthetic_clan.tsv -out exp/fine-tune_bleu.tsv -hf google/flan-t5-xxl -em bleu
