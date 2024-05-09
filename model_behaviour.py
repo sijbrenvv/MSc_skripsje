e91135692b81524f644777eb535cc9cfc2e5ff6c
S4410653 <sijbren.vv@outlook.com>
2024-05-09 22:34:41 +0200

    Update model_behaviour.py: Add test data, clean up code and add error handling

--- model_behaviour.py
+++ model_behaviour.py
@@ -21,9 +21,25 @@ import sys
 from sentence_transformers import SentenceTransformer
 #import sister  # Ref: https://github.com/tofunlp/sister
 from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=UserWarning)
+
+# Use Python logging for logging messages
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-def tokenize_function(examples, **fn_kwargs):
+def tokenize_function(examples, **fn_kwargs: dict[str:any]):
+    """
+    Function to swiftly tokenise the text using the provided tokeniser in fn_kwargs.
+    Args:
+        examples: All the texts that will be tokenised.
+        fn_kwargs (dict): A dictionary of arguments containing the tokeniser, max length value and which column to tokenise.
+    Returns:
+         Input IDs (list of integers): The tokenised examples.
+    """
     return fn_kwargs["tokenizer"](
         examples[fn_kwargs["col"]],
         padding='longest',
@@ -31,9 +47,26 @@ def tokenize_function(examples, **fn_kwargs):
         truncation=True,
         return_tensors="pt")
 
-def get_data(train_path, random_seed):
-    """Function to read the tsv"""
-    train_df = pd.read_csv(train_path, sep='\t', names=['Source', 'Target'], header=0)
+def get_data(train_path: str, test_path: str, random_seed: int) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+    Function to read dataframe with columns.
+    Args:
+        train_path (str): Path to the file containing the train data.
+        test_path (str): Path to the file containing the test data
+    Returns:
+        pd.Series, pd.Series: A series containing the source text and a series containing the target text.
+    """
+    #train_df = pd.read_csv(train_path, sep='\t', names=['Source', 'Target'], header=0)
+    train_df = pd.read_json(train_path, lines=True)
+    test_df = pd.read_json(test_path, lines=True)
+
+    # Rename the columns of the data (train and test) from 'synthetic' and 'preprocessed_text' to 'Source' and 'Target' respectively
+    train_df.rename(columns={"synthetic": "Source", "preprocessed_text": "Target"}, inplace=True)
+    test_df.rename(columns={"synthetic": "Source", "preprocessed_text": "Target"}, inplace=True)
+
+    # We only need the 'Source' and 'Target' columns
+    #train_df = train_df[["Source", "Target"]]
+    #test_df = test_df[["Source", "Target"]]
 
     train_df, val_df = train_test_split(
         train_df,
@@ -41,43 +74,62 @@ def get_data(train_path, random_seed):
         random_state=random_seed,
     )
 
-    return train_df, val_df
+    return train_df, val_df, test_df
 
 
-def con_sent_emb(gen_sen, tar_sen):
-    """ Function to retrieve the contextualised sentence embeddings
-    Input: a list of list of strings
-    Output: a list of list of contextualised sentence embeddings
+def con_sent_emb(gen_sen: list[str], tar_sen: list[str]) -> tuple:
+    """
+    Function to retrieve the contextualised sentence embeddings
+    Args:
+         gen_sen: A list of lists of strings
+         tar_sen: A list of lists of strings
+    Returns:
+        A list of lists of contextual sentence embeddings
     """
     # Perhaps also explore SIF: https://github.com/PrincetonML/SIF
 
     # ref: https://huggingface.co/sentence-transformers/sentence-t5-large
-    model = SentenceTransformer("sentence-transformers/sentence-t5-large")  # "sentence-transformers/sentence-t5-base"
+    senTran = SentenceTransformer("sentence-transformers/sentence-t5-large")  # "sentence-transformers/sentence-t5-base"
     #model = SentenceTransformer("all-MiniLM-L6-v2")  # SentenceBERT
 
     # Generate the contextualised embeddings for the generated and the target completions
-    gen_emb = model.encode(gen_sen)
-    tar_emb = model.encode(tar_sen)
+    gen_emb = senTran.encode(gen_sen)
+    tar_emb = senTran.encode(tar_sen)
 
     return gen_emb, tar_emb
 
 
-def fastText_sent_emb(gen_sen, tar_sen):
-    """ Function to retrieve the static sentence embeddings from FastText"""
+def fastText_sent_emb(gen_sen: list[str], tar_sen: list[str]) -> tuple:
+    """
+    Function to retrieve the static sentence embeddings from FastText
+    Args:
+        gen_sen: All the generated sentences
+        tar_sen: All the target sentences
+    Returns:
+        gen_emb, tar_emb: The FastText static embeddings for the generated and target completions
+    """
     # ref: https://towardsdatascience.com/super-easy-way-to-get-sentence-embedding-using-fasttext-in-python-a70f34ac5b7c
     
     # Initialise the embedder
     embedder = sister.MeanEmbedding(lang="en")
 
-    # Generate the contextualised embeddings for the generated and the target completions
+    # Generate the static embeddings for the generated and the target completions
     gen_emb = embedder(gen_sen)
     tar_emb = embedder(tar_sen)
 
     return gen_emb, tar_emb
 
 
-def compute_metrics(eval_pred, tokenizer, eval_metric):
-    """ """
+def compute_metrics(eval_pred, tokenizer, eval_metric: str) -> dict[str:float]:
+    """
+    Function to calculate the evaluation metric corresponding to a completion.
+    Args:
+        eval_pred: The fragmented sentences and their completions.
+        tokenizer: The model's tokeniser.
+        eval_metric: The evaluation metric to use during fine-tuning.
+    Returns:
+        results (dict): Dictionary containing the metric scores for each completion.
+    """
     # ref: https://colab.research.google.com/drive/1RFBIkTZEqbRt0jxpTHgRudYJBZTD3Szn?usp=sharing#scrollTo=qYq_4DWjdXYa
     ## Different eval metrics and their indexes:
     metric_dic = {
@@ -105,6 +157,7 @@ def compute_metrics(eval_pred, tokenizer, eval_metric):
 
     return results
 
+
 def zero_shot(val_df, model_path, random_seed):
     """ Simple test function to experiment with HF models in a zero shot setting"""
 
@@ -121,7 +174,7 @@ def zero_shot(val_df, model_path, random_seed):
 
     # Use a GPU if available
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print(f"Using device: {device}", file=sys.stderr)
+    logger.info(f"Using device: {device}")
     model.to(device)
 
     # Tokenise using an extensive prefix
@@ -156,7 +209,7 @@ def k_shot(train_df, val_df, model_path, random_seed, k):
 
     # Use a GPU if available
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print(f"Using device: {device}", file=sys.stderr)
+    logger.info(f"Using device: {device}")
     model.to(device)
 
     # the following 2 hyperparameters are task-specific. Set to max values
@@ -194,7 +247,7 @@ def k_shot(train_df, val_df, model_path, random_seed, k):
 
     # Forward pass
     loss = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels).loss
-    print(f"Loss: {loss.item()}", file=sys.stderr)
+    logger.info(f"Loss: {loss.item()}")
 
     # Tokenise test input and generate completions
     tokens = tokenizer(test_dataset['Source'], padding=True, return_tensors="pt", batched=True)
@@ -204,20 +257,14 @@ def k_shot(train_df, val_df, model_path, random_seed, k):
     return tokenizer.batch_decode(output, skip_special_tokens=True)
 
 
-def fine_tune(train_data, checkpoints_path, model_path, eval_metric, random_seed):  # Add 'valid_df' as argument when there is a test set
+def fine_tune(train_data: pd.DataFrame, valid_data: pd.DataFrame,  checkpoints_path: str, model_path: str, eval_metric: str, random_seed):
     """"""
-    # Split the train data into train and dev sets, for the time being
-    train_df, val_df = train_test_split(
-        train_data,
-        test_size=0.2,
-        random_state=random_seed,
-    )
 
     # Pandas dataframe to huggingface Dataset
     #train_dataset = Dataset.from_pandas(train_df.iloc[:])
     #valid_dataset = Dataset.from_pandas(train_df.iloc[:])
-    train_dataset = Dataset.from_pandas(train_df)
-    valid_dataset = Dataset.from_pandas(val_df)
+    train_dataset = Dataset.from_pandas(train_data)
+    valid_dataset = Dataset.from_pandas(valid_data)
 
     # Get tokeniser and model from huggingface
     tokenizer = AutoTokenizer.from_pretrained(model_path)
@@ -230,7 +277,7 @@ def fine_tune(train_data, checkpoints_path, model_path, eval_metric, random_seed
 
     # Use a GPU if available
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print(f"Using device: {device}", file=sys.stderr)
+    logger.info(f"Using device: {device}")
     model.to(device)
 
     # the following  hyperparameter is task-specific. Set to max value
@@ -268,7 +315,7 @@ def fine_tune(train_data, checkpoints_path, model_path, eval_metric, random_seed
         fn_kwargs={"tokenizer": tokenizer, "max_length": max_length, "col": 'Target'},
     )['input_ids']
 
-    # Add the labels to the train dataset
+    # Add the labels to the validation dataset
     tokenized_valid_dataset = tokenized_valid_dataset.to_dict()
     tokenized_valid_dataset["labels"] = valid_labels
     tokenized_valid_dataset = Dataset.from_dict(tokenized_valid_dataset)
@@ -279,7 +326,7 @@ def fine_tune(train_data, checkpoints_path, model_path, eval_metric, random_seed
     # Create Trainer object
     training_args = Seq2SeqTrainingArguments(
         output_dir=checkpoints_path,
-        learning_rate=1e-4,  # 1e-4 or 3e-4 typically work best according to the T5 documentation
+        learning_rate=1e-4,  # 1e-4 or 3e-4 typically works best according to the T5 documentation
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
         num_train_epochs=1,
@@ -304,7 +351,7 @@ def fine_tune(train_data, checkpoints_path, model_path, eval_metric, random_seed
     # Train (fine-tune) the model
     trainer.train()
 
-    # save best model
+    # Save best model
     best_model_path = checkpoints_path + "/best/"
 
     if not os.path.exists(best_model_path):
@@ -313,10 +360,10 @@ def fine_tune(train_data, checkpoints_path, model_path, eval_metric, random_seed
     trainer.save_model(best_model_path)
 
 
-def test(test_df, best_model_path):
+def test(test_data: pd.DataFrame, best_model_path: str) -> list[str]:
     """ """
     # Pandas dataframe to huggingface Dataset
-    test_dataset = Dataset.from_pandas(test_df)
+    test_dataset = Dataset.from_pandas(test_data)
 
     # Get tokeniser from saved model and load best model
     tokenizer = AutoTokenizer.from_pretrained(best_model_path)
@@ -329,7 +376,7 @@ def test(test_df, best_model_path):
 
     # Use a GPU if available
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print(f"Using device: {device}", file=sys.stderr)
+    logger.info(f"Using device: {device}")
     model.to(device)
 
     # the following  hyperparameter is task-specific. Set to max value
@@ -365,8 +412,15 @@ def test(test_df, best_model_path):
 """
 
 
-def evaluate_comp(gen_comp, tar_comp):
-    """ Evaluate the predicted completions against the target completions"""
+def evaluate_comp(gen_comp: list, tar_comp: list) -> dict[str:list]:
+    """
+    Evaluate the predicted completions against the target completions
+    Args:
+        gen_comp (list): All the generated completions.
+        tar_comp (list): All the target completions.
+    Returns:
+        Dictionary: The BLEU, ChrF and Cosine similarity (T5 emb) scores for each sentence pair.
+    """
     bleu = evaluate.load("bleu")
     #meteor = evaluate.load("meteor")
     chrf = evaluate.load("chrf")
@@ -409,6 +463,13 @@ if __name__ == "__main__":
         type=str,
     )
     parser.add_argument(
+        "--test_file_path",
+        "-te",
+        required=True,
+        help="Path to the test file.",
+        type=str,
+    )
+    parser.add_argument(
         "--output_file_path",
         "-out",
         required=True,
@@ -443,23 +504,28 @@ if __name__ == "__main__":
 
     # Set seed for replication
     set_seed(args.random_seed)
-    train_path = args.train_file_path  # For example, 'test_samples.tsv'
-    #prediction_path = args.prediction_file_path  # For example, 'test_predictions.jsonl'
+
+    if not os.path.exists(args.train_file_path):
+        raise FileNotFoundError(f"Train file '{args.train_file_path}' not found.")
+    if not os.path.exists(args.test_file_path):
+        raise FileNotFoundError(f"Test file '{args.test_file_path}' not found.")
+
+    train_path = args.train_file_path  # For example, 'train.json'
+    test_path = args.test_file_path  # For example, 'test.json'
     model = args.huggingface_model  # For example, 'google/flan-t5-small'
-    eval_metric = args.eval_metric  # For example, bleu
+    eval_metric = args.eval_metric  # For example, 'bleu'
 
-    if not os.path.exists(train_path):
-        logging.error(f"File doesnt exists: {train_path}")
-        raise ValueError(f"File doesnt exists: {train_path}")
-
-    # Get the data for the train and test sets
-    train_df, val_df = get_data(train_path, args.random_seed)
+    # Get the data for the train and dev sets
+    logger.info(f"Loading the data...")
+    train_df, val_df, test_df = get_data(train_path, test_path, args.random_seed)
     #gen_comp_zero = zero_shot(val_df, model_path=model, random_seed=args.random_seed)
     #gen_comp_k = k_shot(train_df,val_df,model_path=model,random_seed=args.random_seed,k=1)
 
     # Train completion model
-    fine_tune(  # Add 'valid_df' as argument when there is a test set
+    logger.info(f"Training completion model...")
+    fine_tune(
         train_data=train_df,
+        valid_data=val_df,
         checkpoints_path=f"models/{model}/{args.random_seed}",
         model_path=model,
         eval_metric=eval_metric,
@@ -467,12 +533,13 @@ if __name__ == "__main__":
     )
 
     # Test completion model
-    gen_comp_ft = test(test_df=val_df, best_model_path=f"models/{model}/{args.random_seed}/best/")
-    eval_sc = evaluate_comp(gen_comp=gen_comp_ft, tar_comp=val_df['Target'].to_list())
+    logger.info(f"Testing completion model...")
+    gen_comp_ft = test(test_data=test_df, best_model_path=f"models/{model}/{args.random_seed}/best/")
+    eval_sc = evaluate_comp(gen_comp=gen_comp_ft, tar_comp=test_df['Target'].to_list())
 
     output_df = pd.DataFrame({
-        "Source": val_df['Source'].to_list(),
-        "Target": val_df['Target'].to_list(),
+        "Source": test_df['Source'].to_list(),
+        "Target": test_df['Target'].to_list(),
         "Gen_comp": gen_comp_ft,
         #"Meteor": eval_sc['meteor_sc'],
         "Bleu": eval_sc['bleu_sc'],
@@ -481,5 +548,6 @@ if __name__ == "__main__":
     })
 
     # Export dataframe
+    logger.info(f"Exporting dataframe to '{args.output_file_path}.[json|csv]'...")
     output_df.to_csv(args.output_file_path + ".csv", index=False, sep=',')
     output_df.to_json(args.output_file_path + ".json", orient="records", lines=True)
