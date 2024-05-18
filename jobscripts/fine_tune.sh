41a8503561754dfd163ecaeb446cf03d44274740
S4410653 <sijbren.vv@outlook.com>
2024-05-18 18:49:10 +0200

    Update fine_tune.sh: Add a100 request

--- jobscripts/fine_tune.sh
+++ jobscripts/fine_tune.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=04:00:00
-#SBATCH --gpus-per-node=1
+#SBATCH --gpus-per-node=a100:1
 #SBATCH --partition=gpu
 #SBATCH --mem=86000
 
@@ -34,10 +34,10 @@ data_source=$1
 #python3 fine_tune_t5.py -tr data/${data_source}/${data_source}_train.json -dev data/${data_source}/${data_source}_dev.json -te data/${data_source}/${data_source}_test.json -out exp/${data_source}/fine-tune_meteor -hf google/flan-t5-xxl -em meteor
 
 # ChrF
-python3 fine_tune_t5.py -tr data/${data_source}/${data_source}_train.json -dev data/${data_source}/${data_source}_dev.json -te data/${data_source}/${data_source}_test.json -out exp/${data_source}/fine-tune_chrf -hf google/flan-t5-xl -em chrf
+#python3 fine_tune_t5.py -tr data/${data_source}/${data_source}_train.json -dev data/${data_source}/${data_source}_dev.json -te data/${data_source}/${data_source}_test.json -out exp/${data_source}/fine-tune_chrf -hf google/flan-t5-xl -em chrf
 
 # Google Bleu
-#python3 fine_tune_t5.py -tr data/${data_source}/${data_source}_train.json -dev data/${data_source}/${data_source}_dev.json -te data/${data_source}/${data_source}_test.json -out exp/${data_source}/fine-tune_google_bleu -hf google/flan-t5-xl -em google_bleu
+python3 fine_tune_t5.py -tr data/${data_source}/${data_source}_train.json -dev data/${data_source}/${data_source}_dev.json -te data/${data_source}/${data_source}_test.json -out exp/${data_source}/fine-tune_google_bleu -hf google/flan-t5-xl -em google_bleu
 
 # Backup scripts
 cp fine_tune_t5.py exp/${data_source}/
