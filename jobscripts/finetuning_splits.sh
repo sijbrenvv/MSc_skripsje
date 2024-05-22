e8b50abc44461052496ab62c78635fd53b408352
S4410653 <sijbren.vv@outlook.com>
2024-05-22 18:05:44 +0200

    Update finetuning_splits.sh: Final rules.

--- jobscripts/finetuning_splits.sh
+++ jobscripts/finetuning_splits.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # SBCSAE
-python3 create_finetuning_splits.py -inp exp/rule_base/sbcsae/exp_3word/out/syn_data.json -out data/SBCSAE/
+python3 create_finetuning_splits.py -inp exp/rule_base/sbcsae/exp_upd_rules/out/syn_data.json -out data/SBCSAE/
 
 # Control
-python3 create_finetuning_splits.py -inp exp/rule_base/control/exp_3word/out/syn_data.json -out data/Control/
+python3 create_finetuning_splits.py -inp exp/rule_base/control/exp_upd_rules/out/syn_data.json -out data/Control/
