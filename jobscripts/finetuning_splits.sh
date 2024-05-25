58a077e17992e243b0e015550661bfcbd64d279a
S4410653 <sijbren.vv@outlook.com>
2024-05-25 16:59:15 +0200

    Update finetuning_splits.sh: Capitalise and space removal.

--- jobscripts/finetuning_splits.sh
+++ jobscripts/finetuning_splits.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # SBCSAE
-python3 create_finetuning_splits.py -inp exp/rule_base/sbcsae/exp_upd_rules/out/syn_data.json -out data/SBCSAE/
+python3 create_finetuning_splits.py -inp exp/rule_base/sbcsae/exp_test_2405/out/syn_data.json -out data/SBCSAE/
 
 # Control
-python3 create_finetuning_splits.py -inp exp/rule_base/control/exp_upd_rules/out/syn_data.json -out data/Control/
+python3 create_finetuning_splits.py -inp exp/rule_base/control/exp_test_2405/out/syn_data.json -out data/Control/
