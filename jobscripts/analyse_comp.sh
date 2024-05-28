8deaf0184358644304353f5e1d0ba8c749cdb39b
S4410653 <sijbren.vv@outlook.com>
2024-05-28 21:21:40 +0200

    Update analyse_comp.sh: Analyse all models.

--- jobscripts/analyse_comp.sh
+++ jobscripts/analyse_comp.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 # SBCSAE
-## Without prefix
-python3 analyse_completion.py -inp "exp/completion/SBCSAE/flan-t5-xl/flan-t5-xl_fine-tune_chrf.json" > exp/completion/SBCSAE/flan-t5-xl/analyse_no_pref.txt
-## "Complete this sentence: "
-python3 analyse_completion.py -inp "exp/completion/SBCSAE/flan-t5-xl/flan-t5-xl_pref_fine-tune_chrf.json" > exp/completion/SBCSAE/flan-t5-xl/analyse_cts_txt
+## flan-t5-xl
+### Without prefix
+python3 analyse_completion.py -inp "exp/completion/SBCSAE/flan-t5-xl/flan-t5-xl_nopx_fine-tune_chrf.json" > exp/completion/SBCSAE/flan-t5-xl/analyse_nopx_flan-t5-xl.txt
+### "Complete this sentence: "
+python3 analyse_completion.py -inp "exp/completion/SBCSAE/flan-t5-xl/flan-t5-xl_cts_fine-tune_chrf.json" > exp/completion/SBCSAE/flan-t5-xl/analyse_cts_flan-t5-xl.txt
+
+## flan-t5-base
+### Without prefix
+python3 analyse_completion.py -inp "exp/completion/SBCSAE/flan-t5-base/flan-t5-base_nopx_fine-tune_chrf.json" > exp/completion/SBCSAE/flan-t5-base/analyse_nopx_flan-t5-base.txt
+### "Complete this sentence: "
+python3 analyse_completion.py -inp "exp/completion/SBCSAE/flan-t5-base/flan-t5-base_cts_fine-tune_chrf.json" > exp/completion/SBCSAE/flan-t5-base/analyse_cts_flan-t5-base.txt
+
+## t5-base
+### Without prefix
+python3 analyse_completion.py -inp "exp/completion/SBCSAE/t5-base/t5-base_nopx_fine-tune_chrf.json" > exp/completion/SBCSAE/t5-base/analyse_nopx_t5-base.txt
+### "Complete this sentence: "
+python3 analyse_completion.py -inp "exp/completion/SBCSAE/t5-base/t5-base_cts_fine-tune_chrf.json" > exp/completion/SBCSAE/t5-base/analyse_cts_t5-base.txt
